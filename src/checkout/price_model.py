8141d2cf70dd4104eb199f6536aabcdb65cd1344
riojack <ayjay122@gmail.com>
2019-11-08 09:13:10 -0600

    transformation: PriceModel returns both price and selling method (unit or weight)

--- src/checkout/price_model.py
+++ src/checkout/price_model.py
@@ -3,8 +3,16 @@ class PriceModel:
         self.__product_price_dict = {}
 
     def add_product(self, product_name, product_price):
-        self.__product_price_dict[product_name] = product_price
+        self.__product_price_dict[product_name] = (product_price, sold_by_unit())
         return self
 
     def look_up_product(self, product_name):
-        return self.__product_price_dict.get(product_name, 0.0)
+        return self.__product_price_dict.get(product_name, (0.0, sold_by_unit()))
+
+
+def sold_by_unit():
+    return "sold by unit"
+
+
+def sold_by_weight():
+    return "sold by weight"
--- tests/checkout/test_price_model.py
+++ tests/checkout/test_price_model.py
@@ -1,21 +1,21 @@
-from src.checkout.price_model import PriceModel
+from src.checkout.price_model import PriceModel, sold_by_unit
 
 
-def test_should_save_and_load_pricing_information():
+def test_should_save_and_load_product_information():
     model = PriceModel() \
         .add_product('candy bar', 0.85) \
         .add_product('carrot', 0.15) \
         .add_product('peanuts', 3.25)
 
-    assert model.look_up_product('candy bar') == 0.85
-    assert model.look_up_product('carrot') == 0.15
-    assert model.look_up_product('peanuts') == 3.25
+    assert model.look_up_product('candy bar') == (0.85, sold_by_unit())
+    assert model.look_up_product('carrot') == (0.15, sold_by_unit())
+    assert model.look_up_product('peanuts') == (3.25, sold_by_unit())
 
 
 def test_should_return_zero_if_product_not_found():
     model = PriceModel()
 
-    assert model.look_up_product('apple') == 0.0
+    assert model.look_up_product('apple') == (0.0, sold_by_unit())
 
 
 def test_should_overwrite_product_if_the_same_product_is_added_multiple_times():
@@ -24,4 +24,4 @@ def test_should_overwrite_product_if_the_same_product_is_added_multiple_times():
         .add_product('orange', 0.58) \
         .add_product('orange', 0.53)
 
-    assert model.look_up_product('orange') == 0.53
+    assert model.look_up_product('orange') == (0.53, sold_by_unit())
