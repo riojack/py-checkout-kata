7525a9d8fa21a9ab4c9658f11f4920dbb4be7c2e
riojack <ayjay122@gmail.com>
2019-11-08 09:41:44 -0600

    refactor: rename

--- src/checkout/price_model.py
+++ src/checkout/price_model.py
@@ -1,13 +1,13 @@
 class PriceModel:
     def __init__(self):
-        self.__product_price_dict = {}
+        self.__product_dict = {}
 
     def add_product(self, product_name, product_price):
-        self.__product_price_dict[product_name] = (product_price, sold_by_unit())
+        self.__product_dict[product_name] = (product_price, sold_by_unit())
         return self
 
     def look_up_product(self, product_name):
-        return self.__product_price_dict.get(product_name, (0.0, sold_by_unit()))
+        return self.__product_dict.get(product_name, (0.0, sold_by_unit()))
 
 
 def sold_by_unit():
